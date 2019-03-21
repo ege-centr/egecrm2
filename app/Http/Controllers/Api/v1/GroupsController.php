115a949fe46a3b026358aff92ca84d8ffb6b334b
maxflex <>
2019-03-21 13:45:42 +0300

    Groups index update

--- app/Http/Controllers/Api/v1/GroupsController.php
+++ app/Http/Controllers/Api/v1/GroupsController.php
@@ -12,7 +12,7 @@ class GroupsController extends Controller
 {
     protected $filters = [
         'multiple' => ['year', 'teacher_id', 'subject_id', 'grade_id'],
-        'equals' => ['client_id'],
+        'equals' => ['client_ids'],
     ];
 
     public function index(Request $request)
