b5345277f1910d405cc0ad266fad29a80c4d1879
maxflex <>
2019-04-25 18:00:14 +0300

    Spaces added

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,3 @@
 <?php
-
-Route::get('/store', function () {
-    $path = config('app.env') . '/' . date('Y-m-d') . '.json'; // local/2019-03-01.json
-    Storage::disk('spaces')->put($path, json_encode(['foo' => 'bar']));
-});
-
 Route::get('download/{id}', 'DownloadController@download');
 Route::get('/{any}', 'AppController@index')->where('any', '.*');
