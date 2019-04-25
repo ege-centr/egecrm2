163798a0e3a1af294318f7b67324e1557b056c23
maxflex <>
2019-04-25 16:16:35 +0300

    Favicon update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Личный кабинет</title>
     <link rel="stylesheet" href="{{ mix('css/app.css')}} ">
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <link rel="icon" type="image/png" href="favicon.png">
     <script src="https://js.pusher.com/4.3/pusher.min.js"></script>
 </head>
 <body>
