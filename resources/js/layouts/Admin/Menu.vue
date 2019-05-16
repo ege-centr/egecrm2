fc40c95fc9ee50de1154e8b7244a3c0a52a8c7c8
maxflex <>
2019-05-16 15:45:32 +0300

    whitespace removed

--- resources/js/layouts/Admin/Menu.vue
+++ resources/js/layouts/Admin/Menu.vue
@@ -25,7 +25,9 @@
         <v-icon>exit_to_app</v-icon>
       </v-list-tile-action>
       <v-list-tile-content>
-        <v-list-tile-title>Выход</v-list-tile-title>
+        <v-list-tile-title>
+          Выход
+        </v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile> 
   </v-list>
