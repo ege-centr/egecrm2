8a785b0cf60ac334b0ebc3c30d70e60400eaf0bd
maxflex <>
2019-05-17 17:57:18 +0300

    Mess fixes

--- app/Http/Controllers/Api/v1/ReportsController.php
+++ app/Http/Controllers/Api/v1/ReportsController.php
@@ -23,7 +23,7 @@ class ReportsController extends Controller
             'facets' => ['*']
         ]);
         $this->filter($request, $query);
-        $result = new AlgoliaResult($query->paginateRaw($request->paginate));
+        $result = new AlgoliaResult($query->paginateRaw($request->paginate ?: SHOW_ALL));
         $result->getCollection()->transform(function ($items, $key) {
             if ($key === 'hits') {
                 foreach($items as &$item) {
