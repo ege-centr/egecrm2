9ddb632fec474732416d82984d52edd7f2ce77d3
maxflex <>
2019-05-23 18:23:45 +0300

    Client color in ConductDialog

--- app/Http/Resources/Lesson/LessonResource.php
+++ app/Http/Resources/Lesson/LessonResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Lesson;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\Person\PersonResource;
 use App\Http\Resources\Teacher\TeacherCollection;
+use App\Http\Resources\Client\ClientWithSubjectStatus;
 
 class LessonResource extends JsonResource
 {
@@ -15,8 +16,13 @@ class LessonResource extends JsonResource
             $clientLesson->client->subject_status = $clientLesson->client->getSubjectStatus($this->group->year, $this->group->subject_id);
         }
 
+        $groupClients = $this->group->clients;
+        foreach($groupClients as &$groupClient) {
+            $groupClient->subject_status = $groupClient->getSubjectStatus($this->group->year, $this->group->subject_id);
+        }
+
         return array_merge(parent::toArray($request), [
-            'groupClients' => PersonResource::collection($this->group->clients),
+            'groupClients' => ClientWithSubjectStatus::collection($groupClients),
             'createdUser' => new PersonResource($this->createdUser),
             'conductedUser' => new PersonResource($this->conductedUser),
             'clientLessons' => ClientLessonInSchedule::collection($this->clientLessons),
