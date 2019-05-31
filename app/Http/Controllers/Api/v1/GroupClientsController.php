f8b2c017dcd19be238ec0bfee79b5bfe07cbcefc
maxflex <>
2019-05-31 13:35:48 +0300

    updaets

--- app/Http/Controllers/Api/v1/GroupClientsController.php
+++ app/Http/Controllers/Api/v1/GroupClientsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Group\GroupClient;
+use App\Models\Group\{Group, GroupClient};
 
 class GroupClientsController extends Controller
 {
