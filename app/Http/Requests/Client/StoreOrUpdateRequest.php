7d9619f8211dce05b5f92b5f1f4aa3aa383eb010
maxflex <>
2019-06-24 13:21:09 +0300

    Representative email different

--- app/Http/Requests/Client/StoreOrUpdateRequest.php
+++ app/Http/Requests/Client/StoreOrUpdateRequest.php
@@ -6,34 +6,32 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreOrUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
             'first_name' => ['required'],
             'phones.*.phone' => ['required', 'phone'],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', 'different:representative.email'],
 
             'representative.first_name' => ['required'],
             'representative.phones.*.phone' => ['required', 'phone'],
-            'representative.email' => ['required', 'email'],
+            'representative.email' => ['required', 'email', 'different:email'],
 
             'grade_id' => ['required'],
             'year' => ['required'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.different' => 'Одинаковый email у ученика и представителя',
+            'representative.email.different' => 'Одинаковый email у ученика и представителя',
         ];
     }
 }
