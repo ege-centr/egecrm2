5f2bc8f672aa06c350f36cd4ab16e18545c31125
maxflex <>
2019-06-24 17:16:36 +0300

    updates before final transfer

--- app/Console/Commands/Transfer/TransferCommand.php
+++ app/Console/Commands/Transfer/TransferCommand.php
@@ -32,9 +32,9 @@ class TransferCommand extends Command
             return $admin->email->id;
         } else {
             // не нашелся админ – пытаемся найти по учителю
-            $teacher = dbEgecrm('users')->whereId('id', $userId)->where('type', 'TEACHER')->first();
+            $teacher = dbEgecrm('users')->whereId('id_entity', $userId)->where('type', 'TEACHER')->first();
             if ($teacher) {
-                return optional(Teacher::find($teacher->id_entity)->email)->id;
+                return optional(Teacher::find($userId)->email)->id;
             }
         }
         return null;
