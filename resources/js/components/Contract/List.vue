0965684b50b636bf3ff6bfbd4902c88de10e23b0
maxflex <>
2019-07-03 14:56:02 +0300

    Client photos

--- resources/js/components/Contract/List.vue
+++ resources/js/components/Contract/List.vue
@@ -97,6 +97,14 @@
                       <v-list-tile-title>Договор</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
+                <v-list-tile @click="print(item, 'maternal-capital')">
+                    <v-list-tile-action>
+                      <v-icon>print</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Дополнение на маткапитал</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
                 <v-list-tile @click="print(item, 'additional-agreement')">
                     <v-list-tile-action>
                       <v-icon>print</v-icon>
