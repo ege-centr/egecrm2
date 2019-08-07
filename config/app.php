5f53328e8420107328ef065225ab6a1cee2ec6b6
maxflex <>
2019-08-07 15:19:28 +0300

    APP_HOST removed

--- config/app.php
+++ config/app.php
@@ -55,7 +55,6 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'host' => env('APP_HOST'),
 
     'egerep-url' => env('EGEREP_URL'),
     'spaces-url' => env('DO_SPACES_URL'),
